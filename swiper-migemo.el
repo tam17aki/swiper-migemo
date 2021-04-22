b9e3170ed348a9bb048f35aa995febc902a4758b
Akira Tamamori <tamamori5917@gmail.com>
2021-04-23 01:14:52 +0900

    fix docstrings.

--- swiper-migemo.el
+++ swiper-migemo.el
@@ -19,14 +19,14 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
 ;; In the past, many people used avy-migemo to support migemo in swiper, ivy and
-;; counsel. However, avy-migemo has not been maintained for a long time, and
+;; counsel.  However, avy-migemo has not been maintained for a long time, and
 ;; many attempts have been made to achieve migemo support without using
-;; avy-migemo. I decided to write a new minor-mode to make those commands with
+;; avy-migemo.  I decided to write a new minor-mode to make those commands with
 ;; ivy-based interface compatible with migemo.
 
 ;;; Installation:
@@ -54,7 +54,7 @@
 ;; (migemo-init)
 ;;
 ;; Note: The above settings must be evaluated before the activataion of this
-;; minor-mode. That is,
+;; minor-mode.  That is,
 ;;
 ;; (require 'swiper-migemo)
 ;; (add-to-list 'swiper-migemo-enable-command 'counsel-recentf)
@@ -80,7 +80,7 @@
   :prefix "swiper-migemo-")
 
 (defcustom swiper-migemo-lighter " SWM" ; Swiper-With-Migemo
-  "Lighter for `swiper-migemo-mode'."
+  "Lighter for the minor-mode."
   :type '(choice (const :tag "Not displayed." nil)
                  string)
   :group 'swiper-migemo)
